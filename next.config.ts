0198c30eb3760433e4d1bebebef1d1ef5740063f
Pratham Sharma <pratham@captureatrip.com>
2025-07-13 21:13:32 +0530

    Updated next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+    images: {
+    remotePatterns: [
+      // https://captureatrip-cms-storage.s3.ap-south-1.amazonaws.com
+      {
+        protocol: "https",
+        hostname: "v2-statics.s3.ap-south-1.amazonaws.com",
+        pathname: "/**",
+      }
+      ]
+    }
 };
 
 export default nextConfig;
