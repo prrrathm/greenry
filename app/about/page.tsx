b22bdb569b10546ffbd5f01f9c858d385553e8ce
prrrathm <prrrathm@gmail.com>
2025-09-28 18:26:32 +0530

    update: remove leadership part

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -262,7 +262,7 @@ export default function AboutPage() {
 			</section>
 
 			{/* Founder Section */}
-			<section className="py-20 bg-white">
+			<section className="py-20 bg-white hidden">
 				<div className="container mx-auto px-4 sm:px-6 lg:px-8">
 					<div className="max-w-4xl mx-auto">
 						<div className="text-center mb-16">
